895635794c0a4e9df2d7d234867aca37905693d8
diegosebastian2 <diegosebastian2@gmail.com>
2018-05-10 11:21:30 -0300

    first

---
+++ Indemnizaciones/conexion/conexion.php
@@ -0,0 +1,7 @@
+<?php 
+
+$conexion = mysqli_connect("localhost","root","","lupulo") or die("Error al conectarse a la base de datos");
+
+mysqli_set_charset($conexion, "utf8");
+
+ ?>
---
+++ Indemnizaciones/index.php
@@ -0,0 +1,189 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Indemnizaciones aereas</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="favicon.ico">
+        <link rel="icon" href="favicon.ico">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+        <link rel="stylesheet" href="css/custom.css">
+    </head>
+    <body>
+      <div class="content" id="content">
+        <header class="mainhead sticky-top" >
+
+          <div class="logo">
+
+            <div class="logoimg">
+              <a href="index.html">
+                <h1>Indemnizacionesaereasya</h1>
+              </a>
+            </div>
+
+            <div class="logotext">
+              <a href="index.html">
+                <h1>Indemnizaciones<span class="aereas">Aereas</span><span class="ya">Ya</span></h1>
+              </a>
+            </div>
+
+          </div>
+
+          <nav class="mainnav align-items-center" aria-label="Site navigation">
+              <ul class="pagination justify-content-center mb-0 nav">
+                <li class="page-item"><a class="page-link border-0 active" href="#inicio">Inicio</a></li>
+                <li class="page-item"><a class="page-link border-0 " href="#acerca">Quienes Somos</a></li>
+                <li class="page-item"><a class="page-link border-0 " href="#procedimiento">Operatoria del reclamo</a></li>
+                <li class="page-item"><a class="page-link border-0 " href="#contacto">Contacto</a></li>
+              </ul>
+          </nav>
+
+        </header>
+
+        <div id="home" >
+
+
+            <div class="slider">
+              <div class="slide1">
+                <p class="item-slide">¿Equipaje perdido y/o dañado?</p>
+                <p class="item-slide">¿Sobreventa de pasajes (overbooking)?</p>
+              </div>
+              <div class="slide2">
+                <p class="item-slide">¿Cancelaron o retrasaron tu vuelo?</p>
+                <p class="item-slide">¿Perdiste tu conexión?</p>
+              </div>
+              <div class="slide3">
+                <p class="item-slide">¿Problemas con las reservas del hotel o excursiones?</p>
+                <p class="item-slide">¿Tuviste lesiones a bordo de la aeronave?</p>
+              </div>
+              <div class="slide4">
+                <p class="item-slide">TENES DERCHO A SER INDEMNIZADO!</p>
+                <p class="item-slide">...y nosotros nos ocupamos de ello!</p>
+              </div>
+
+
+            </div>
+
+
+
+
+        </div>
+
+        <div id="acerca" class="acerca">
+
+            <section class="acerca" >
+              <div class="container" >
+                <div class="row align-items-center">
+                  <div class="col-12">
+                    <div class="p-5">
+                      <h2 class="h2">QUIENES SOMOS?</h2>
+                      <p>UN ESTUDIO JURIDICO ESPECIALIZADO en DAÑOS Y PERJUICIOS, defensa del consumidor, ACCIDENTES DE TRANSITO Y DERECHO AERONÁUTICO
+                        Los viajes de negocios y/o de placer, cada vez con mayor frecuencia, suelen ser opacados y en muchos casos arruinados por situaciones ajenas e inesperadas que las compañías aéreas suelen provocar. Entre ellas las más comunes son vuelos con demoras, cancelaciones, pérdida de equipaje y/o hasta el robo del mismo. Obviamente, dichas compañías suelen negar los hechos o ampararse en excusas tales como “malas condiciones climáticas”, “problemas mecánicos”, etc, con el fin de evitar pagar al usuario la indemnización correspondiente por el perjuicio ocasionado directamente por ellas.</p>
+                      <p>La finalidad de “Indemnizacionesaéreas.com” es que puedas recibir el resarcimiento económico que te corresponde como consecuencia de las situaciones ocasionadas por estas compañías, realizando para tal fin, todos los reclamos extrajudiciales y/o judiciales necesarios en el menor tiempo posible.</p>
+                      <p>La experiencia que tenemos y brindamos, en este ámbito del derecho aeronáutico, nos permite fundamentar con más recursos que la que puede tener una persona común y corriente.</p>
+                      <p>Y lo más importante es que, no solo te ahorramos tramites interminables y el estrés que ocasionan estos reclamos, sino que NO PAGAS HONORARIOS HASTA TANTO HAYAS RECIBIDO TU MERECIDA INDEMNIZACIÓN.</p>
+                      <p>No cobramos gastos previos. </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+        </div>
+
+        <div id="procedimiento" class="procedimiento">
+
+            <section class="procedimiento" >
+              <div class="container" >
+                <div class="row align-items-center">
+                  <div class="col-12">
+                    <div class="p-5">
+                      <h2 class="h2">Como es el procedimiento del reclamo?</h2>
+                        <p>Si debido a circunstancias operativas, técnicas, comerciales y hasta en algunos casos climáticas, la aerolínea demora y/o cancela tu vuelo en más de CUATRO (4) horas, te prohíbe el embarque aludiendo que no dispone de espacio previamente contratado y confirmado por sobreventa de pasajes, o no te entrega tu equipaje en tiempo y forma, y/o te causa la pérdida de un vuelo de conexión para el que tenías una reserva confirmada o perdida de estadía en tu lugar de destino, la Compañía Aérea deberá indemnizarte por todos los daños y perjuicios ocasionados, incluyendo no solo los gastos incurridos , sino también el daño moral causado.</p>
+                        <p>La indemnización por la que vamos a trabajar debe contemplar también la pérdida material y de tiempo sufrida por la pérdida de vida irrepetible.</p>
+                        <p>La vía para pedir la indemnización es primero una mediación extrajudicial y posteriormente la demanda judicial en el menor tiempo posible!</p>
+                        <p>La experiencia y confianza en este tipo de litigios aeronáuticos y daños y perjuicios nos respalda!</p>
+                        <p>Haga valer sus derechos y el de su familia.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+        </div>
+
+      </div>
+    <!-- Footer -->
+    <footer class="py-1">
+        <p class="text-center text-white small">Copyright &copy; Your Website 2018</p>
+      <!-- /.container -->
+    </footer>
+
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <!--<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>-->
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+        <script src="js/jquery.fancybox.min.js"></script>
+        <script src="js/jquery.zoom.min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+
+        <script>
+        $(document).ready(function(){
+
+                $('a.preparacionimg').fancybox({
+                    afterLoad : function(instance, current) {
+                        var pixelRatio = window.devicePixelRatio || 1;
+
+                        if ( pixelRatio > 1.5 ) {
+                            current.width  = current.width  / pixelRatio;
+                            current.height = current.height / pixelRatio;
+                        }
+                    }
+                });
+
+
+                $('img.variedades').wrap('<span style="display:inline-block"></span>')
+                .css('display', 'block')
+                .parent()
+                .zoom({
+                    on: 'grab',
+                    duration:200,
+                    touch: true
+                });
+
+
+                $('nav ul li a').attr("data-scroll");
+
+        });
+
+
+        var scroll = new SmoothScroll("a[href*='#']",{
+
+        });
+        </script>
+
+
+
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
