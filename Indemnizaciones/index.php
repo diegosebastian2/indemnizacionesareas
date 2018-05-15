d917c6bb4685711d5b7c476bf01e62bcc1bd12bd
diegosebastian2 <diegosebastian2@gmail.com>
2018-05-15 10:08:25 -0300

    hp to desk

--- Indemnizaciones/index.php
+++ Indemnizaciones/index.php
@@ -126,14 +126,14 @@
               <div class="container contacto ">
 
 
-                  <div class="p-5 texto ">
+                  <div class="pt-5 pl-5 texto ">
                       <h2 class="h2 pb-4">Contactanos</h2>
                         <p>Dirección:
                         Lavalle 1290 Piso 12 CP 1048 (CABA)</p>
                         <p>Cel: (011) 155-479-0213.</p>
                         <p>Cel: (02324) 1569-3961.</p>
                     </div>
-                    <div class="p-5 maps">
+                    <div class="py-5 maps">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.0594182075733!2d-58.38723248477043!3d-34.60265898045974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccac66eff3dd9%3A0x4eef77fc4871345!2sPiso+12%2C+Lavalle+1290%2C+C1048AAF+CABA!5e0!3m2!1ses-419!2sar!4v1525978951892" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </div>
               </div>
